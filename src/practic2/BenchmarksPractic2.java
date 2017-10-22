d23c06813814ac0ef1843fbd563af3d6f542e76e
ilgarsh <ilgar.shahbazov@gmail.com>
2017-10-22 16:38:46 +0300

    practic2 - add streamapi

--- src/practic2/BenchmarksPractic2.java
+++ src/practic2/BenchmarksPractic2.java
@@ -13,12 +13,13 @@ import practic2.concurrency.algorithms.SequentialSearchAlgorithm;
 import practic2.concurrency.algorithms.ThreadPoolAlgorithm;
 
 import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 public class BenchmarksPractic2 {
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
     public int[] m1_DecompositionNumbersAlgorithm() {
         return FinderPrimes.INSTANCE
                 .find(10_000, DecompositionNumbersAlgorithm.INSTANCE.withNThreads(10));
@@ -26,7 +27,7 @@ public class BenchmarksPractic2 {
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
     public int[] m2_DecompositionPrimesAlgorithm() {
         return FinderPrimes.INSTANCE
                 .find(10_000, DecompositionPrimesAlgorithm.INSTANCE.withNThreads(10));
@@ -34,7 +35,7 @@ public class BenchmarksPractic2 {
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
     public int[] m3_ThreadPoolAlgorithm() {
         return FinderPrimes.INSTANCE
                 .find(10_000, ThreadPoolAlgorithm.INSTANCE);
@@ -42,7 +43,7 @@ public class BenchmarksPractic2 {
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
     public int[] m4_SequentialSearchAlgorithm() {
         return FinderPrimes.INSTANCE
                 .find(10_000, SequentialSearchAlgorithm.INSTANCE.withNThreads(10));
@@ -50,7 +51,16 @@ public class BenchmarksPractic2 {
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public IntStream m5_StreamAPI() {
+        return IntStream.rangeClosed(2, 10_000)
+                .filter(i -> IntStream.rangeClosed(2, (int)Math.sqrt(i))
+                        .allMatch(j -> i%j != 0));
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
     public int[] StandartAlgorithm() {
         return EratosthenesAlgorithm.getAllPrimes(10_000);
     }
